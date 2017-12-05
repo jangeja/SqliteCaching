b42ecc6ad38bb48ff8f1f3bc79bb802d821b98da
Joey Angeja <angeja16@gmail.com>
2017-12-04 21:18:40 -0800

    all algos except second chance

--- Development/test/speedTest.cpp
+++ Development/test/speedTest.cpp
@@ -10,9 +10,9 @@ using namespace std;
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
     int i;
     for (i = 0; i < argc; i++) {
-        //printf, ("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+        // printf ("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
     }
-    //printf("\n");
+    // printf("\n");
     return 0;
 }
 
